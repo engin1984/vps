f799e7030046c5077b16c47d503cc9d6cfa1bd82
EnginS <m@e-ng.in>
2025-05-20 17:36:53 +0300

    font

--- _main.js
+++ _main.js
@@ -48,21 +48,37 @@ darkToggleBtn?.addEventListener('click', () => {
 
 
 // ############ https://bulma.io/documentation/components/message/
+ function setCookie(name, value, days) {
+    const expires = new Date(Date.now() + days*864e5).toUTCString();
+    document.cookie = name + '=' + encodeURIComponent(value) + '; expires=' + expires + '; path=/';
+  }
 
-document.addEventListener('DOMContentLoaded', () => {
-  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
-    const $notification = $delete.parentNode;
+  function getCookie(name) {
+    return document.cookie.split('; ').reduce((r, v) => {
+      const parts = v.split('=');
+      return parts[0] === name ? decodeURIComponent(parts[1]) : r;
+    }, '');
+  }
 
-    console.log('Found delete button:', $delete);
+  document.addEventListener('DOMContentLoaded', () => {
+    if (getCookie('siteCookieNotice') === 'dismissed') {
+      const el = document.querySelector('.notification.is-info.is-light');
+      if (el) el.remove();
+      return;
+    }
 
-    $delete.addEventListener('click', () => {
-      console.log('Clicked delete');
-      $notification.remove(); // Modern kullanım
+    (document.querySelectorAll('.notification .delete') || []).forEach($delete => {
+      const $notification = $delete.parentNode;
+      console.log('Found delete button:', $delete);
+
+      $delete.addEventListener('click', () => {
+        console.log('Clicked delete');
+        setCookie('siteCookieNotice', 'dismissed', 365);
+        $notification.remove();
+      });
     });
   });
-});
-
-
+// ############ 
 
 
 
